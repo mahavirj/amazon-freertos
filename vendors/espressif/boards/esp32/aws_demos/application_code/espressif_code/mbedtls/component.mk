fb8105186940009ca6c0aa383e2d87b4b405f3e2
Mahavir Jain <mahavir@espressif.com>
2019-07-09 12:58:16 +0530

    Build fixes with ESP-IDF v3.2.2 release
    Signed-off-by: Mahavir Jain <mahavir@espressif.com>

--- vendors/espressif/boards/esp32/aws_demos/application_code/espressif_code/mbedtls/component.mk
+++ vendors/espressif/boards/esp32/aws_demos/application_code/espressif_code/mbedtls/component.mk
@@ -2,5 +2,5 @@ AMAZON_FREERTOS_LIB_DIR := ../../../../../../../../libraries
 
 COMPONENT_SRCDIRS := $(AMAZON_FREERTOS_LIB_DIR)/3rdparty/mbedtls/library port
 
-COMPONENT_ADD_INCLUDEDIRS := $(AMAZON_FREERTOS_LIB_DIR)/3rdparty/mbedtls/include port/include
+COMPONENT_ADD_INCLUDEDIRS := port/include $(AMAZON_FREERTOS_LIB_DIR)/3rdparty/mbedtls/include
 
